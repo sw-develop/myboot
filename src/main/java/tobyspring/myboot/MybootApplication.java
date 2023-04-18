97a3049a07364098268b9fc3f107dc54335d1faf
박세원B <sewonpark@smilegate.sg>
2023-04-18 16:20:44 +0900

    add: 프론트 컨트롤러로 전환

--- src/main/java/tobyspring/myboot/MybootApplication.java
+++ src/main/java/tobyspring/myboot/MybootApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactor
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 
@@ -17,15 +18,24 @@ public class MybootApplication {
 
     public static void main(String[] args) {
         ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
-        WebServer webServer = serverFactory.getWebServer(servletContext -> servletContext.addServlet("hello", new HttpServlet() {
+        WebServer webServer = serverFactory.getWebServer(servletContext -> servletContext.addServlet("FrontController", new HttpServlet() {
             @Override
             protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-                String name = req.getParameter("name");
-                resp.setStatus(HttpStatus.OK.value());
-                resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
-                resp.getWriter().println("Hello " + name);
+                // 프론트 컨트롤러는 공통 작업을 수행하고, 이후에 요청을 매핑함
+                if (req.getRequestURI().equals("/hello") && req.getMethod().equals(HttpMethod.GET.name())) {
+                    String name = req.getParameter("name");
+                    resp.setStatus(HttpStatus.OK.value());
+                    resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
+                    resp.getWriter().println("Hello " + name);
+                } else if (req.getRequestURI().equals("/user")) {
+                    resp.setStatus(HttpStatus.OK.value());
+                    resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
+                    resp.getWriter().println("user");
+                } else {
+                    resp.setStatus(HttpStatus.NOT_FOUND.value());
+                }
             }
-        }).addMapping("/hello"));
+        }).addMapping("/*")); //모든 URL 처리하도록 서블릿 바인딩 변경
         webServer.start();
     }
 }
