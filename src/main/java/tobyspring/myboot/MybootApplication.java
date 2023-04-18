c595598b78274d7bd650e3f5af3030f3a6c0bea5
박세원B <sewonpark@smilegate.sg>
2023-04-18 14:40:38 +0900

    add: 서블릿 컨테이너 띄우기

--- src/main/java/tobyspring/myboot/MybootApplication.java
+++ src/main/java/tobyspring/myboot/MybootApplication.java
@@ -1,13 +1,15 @@
 package tobyspring.myboot;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServer;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 
-@SpringBootApplication
 public class MybootApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(MybootApplication.class, args);
+        ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+        WebServer webServer = serverFactory.getWebServer();
+        webServer.start();
     }
 
 }
