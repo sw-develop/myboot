c0da66676515456b3e65943cf724efa39ef4d7d1
박세원B <sewonpark@smilegate.sg>
2023-04-19 18:07:46 +0900

    add: ServletWebServerFactory와 DispatcherServlet을 빈으로 등록

--- src/main/java/tobyspring/myboot/MybootApplication.java
+++ src/main/java/tobyspring/myboot/MybootApplication.java
@@ -3,6 +3,7 @@ package tobyspring.myboot;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
@@ -10,17 +11,27 @@ import org.springframework.web.servlet.DispatcherServlet;
 @ComponentScan
 public class MybootApplication {
 
+    @Bean
+    public ServletWebServerFactory serverFactory() {
+        return new TomcatServletWebServerFactory();
+    }
+
+    @Bean
+    public DispatcherServlet dispatcherServlet() {
+        return new DispatcherServlet();
+    }
     public static void main(String[] args) {
         AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext() {
             @Override
             protected void onRefresh() {
                 super.onRefresh();
 
-                ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+                ServletWebServerFactory serverFactory = this.getBean(ServletWebServerFactory.class);
+                DispatcherServlet dispatcherServlet = this.getBean(DispatcherServlet.class);
+
                 WebServer webServer = serverFactory.getWebServer(servletContext ->
-                        servletContext.addServlet("dispatcherServlet",
-                                new DispatcherServlet(this) //WebApplicationContext 전달
-                        ).addMapping("/*"));
+                        servletContext.addServlet("dispatcherServlet", dispatcherServlet)
+                                .addMapping("/*"));
                 webServer.start();
             }
         };
