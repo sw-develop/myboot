88010273e3609758a5629fb552df18df2766f31b
박세원B <sewonpark@smilegate.sg>
2023-04-19 10:32:19 +0900

    add: 스프링 컨테이너 사용

--- src/main/java/tobyspring/myboot/MybootApplication.java
+++ src/main/java/tobyspring/myboot/MybootApplication.java
@@ -3,7 +3,7 @@ package tobyspring.myboot;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.http.HttpHeaders;
+import org.springframework.context.support.GenericApplicationContext;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -17,31 +17,29 @@ import java.io.IOException;
 public class MybootApplication {
 
     public static void main(String[] args) {
+        //Spring Container & Bean 등록
+        GenericApplicationContext applicationContext = new GenericApplicationContext();
+        applicationContext.registerBean(HelloController.class); //빈 등록
+        applicationContext.refresh();
+
+        //Servlet Container & Servlet 등록
         ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
-        WebServer webServer = serverFactory.getWebServer(servletContext -> {
-            HelloController helloController = new HelloController();
+        WebServer webServer = serverFactory.getWebServer(servletContext -> servletContext.addServlet("FrontController", new HttpServlet() {
+            @Override
+            protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+                if (req.getRequestURI().equals("/hello") && req.getMethod().equals(HttpMethod.GET.name())) { //매핑
+                    String name = req.getParameter("name");
 
-            servletContext.addServlet("FrontController", new HttpServlet() {
-                @Override
-                protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-                    if (req.getRequestURI().equals("/hello") && req.getMethod().equals(HttpMethod.GET.name())) { //매핑
-                        String name = req.getParameter("name");
+                    HelloController helloController = applicationContext.getBean(HelloController.class);
+                    String ret = helloController.hello(name); //바인딩
 
-                        String ret = helloController.hello(name); //바인딩
-
-                        resp.setStatus(HttpStatus.OK.value());
-                        resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
-                        resp.getWriter().println(ret);
-                    } else if (req.getRequestURI().equals("/user")) {
-                        resp.setStatus(HttpStatus.OK.value());
-                        resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
-                        resp.getWriter().println("user");
-                    } else {
-                        resp.setStatus(HttpStatus.NOT_FOUND.value());
-                    }
+                    resp.setContentType(MediaType.TEXT_PLAIN_VALUE);
+                    resp.getWriter().println(ret);
+                } else {
+                    resp.setStatus(HttpStatus.NOT_FOUND.value());
                 }
-            }).addMapping("/*");
-        });
+            }
+        }).addMapping("/*"));
         webServer.start();
     }
 }
