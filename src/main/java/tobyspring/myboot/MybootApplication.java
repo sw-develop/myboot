2eb5654577f90f8c4ec1a2c6c2cf6e04f1ccb4f9
박세원B <sewonpark@smilegate.sg>
2023-04-19 16:29:07 +0900

    add: 스프링 컨테이너로 통합

--- src/main/java/tobyspring/myboot/MybootApplication.java
+++ src/main/java/tobyspring/myboot/MybootApplication.java
@@ -9,18 +9,23 @@ import org.springframework.web.servlet.DispatcherServlet;
 public class MybootApplication {
 
     public static void main(String[] args) {
-        //Spring Container & Bean 등록
-        GenericWebApplicationContext applicationContext = new GenericWebApplicationContext();
+        GenericWebApplicationContext applicationContext = new GenericWebApplicationContext() {
+            @Override
+            protected void onRefresh() {
+                super.onRefresh();
+
+                //Servlet Container 초기화 & Servlet 등록
+                ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+                WebServer webServer = serverFactory.getWebServer(servletContext ->
+                        servletContext.addServlet("dispatcherServlet",
+                                new DispatcherServlet(this) //WebApplicationContext 전달
+                        ).addMapping("/*"));
+                webServer.start();
+            }
+        };
+
         applicationContext.registerBean(HelloController.class); //빈 등록
         applicationContext.registerBean(SimpleHelloService.class);
         applicationContext.refresh();
-
-        //Servlet Container & Servlet 등록
-        ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
-        WebServer webServer = serverFactory.getWebServer(servletContext ->
-                servletContext.addServlet("dispatcherServlet",
-                        new DispatcherServlet(applicationContext) //스프링 컨테이너를 사용할 수 있도록 ApplicationContext 전달
-                ).addMapping("/*"));
-        webServer.start();
     }
 }
