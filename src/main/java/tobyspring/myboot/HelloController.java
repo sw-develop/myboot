e806441dcbc2a4305e28be4767a7efeb041550e8
박세원B <sewonpark@smilegate.sg>
2023-04-19 10:55:36 +0900

    add: SimpleHelloService 추가

--- src/main/java/tobyspring/myboot/HelloController.java
+++ src/main/java/tobyspring/myboot/HelloController.java
@@ -1,10 +1,11 @@
 package tobyspring.myboot;
 
+import java.util.Objects;
+
 public class HelloController {
 
-    public HelloController() {}
-
     public String hello(String name) {
-        return "Hello " + name;
+        SimpleHelloService helloService = new SimpleHelloService();
+        return helloService.sayHello(Objects.requireNonNull(name));
     }
 }
---
+++ src/main/java/tobyspring/myboot/SimpleHelloService.java
@@ -0,0 +1,8 @@
+package tobyspring.myboot;
+
+public class SimpleHelloService {
+
+    public String sayHello(String name) {
+        return "Hello " + name;
+    }
+}
