d49abf013646fcddddb329b1e0d9a519c1938667
박세원B <sewonpark@smilegate.sg>
2023-04-19 11:38:39 +0900

    add: DispatcherServlet으로 전환

--- src/main/java/tobyspring/myboot/MybootApplication.java
+++ src/main/java/tobyspring/myboot/MybootApplication.java
@@ -3,44 +3,24 @@ package tobyspring.myboot;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.context.support.GenericApplicationContext;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
+import org.springframework.web.context.support.GenericWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
 
 public class MybootApplication {
 
     public static void main(String[] args) {
         //Spring Container & Bean 등록
-        GenericApplicationContext applicationContext = new GenericApplicationContext();
+        GenericWebApplicationContext applicationContext = new GenericWebApplicationContext();
         applicationContext.registerBean(HelloController.class); //빈 등록
         applicationContext.registerBean(SimpleHelloService.class);
         applicationContext.refresh();
 
         //Servlet Container & Servlet 등록
         ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
-        WebServer webServer = serverFactory.getWebServer(servletContext -> servletContext.addServlet("FrontController", new HttpServlet() {
-            @Override
-            protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-                if (req.getRequestURI().equals("/hello") && req.getMethod().equals(HttpMethod.GET.name())) { //매핑
-                    String name = req.getParameter("name");
-
-                    HelloController helloController = applicationContext.getBean(HelloController.class);
-                    String ret = helloController.hello(name); //바인딩
-
-                    resp.setContentType(MediaType.TEXT_PLAIN_VALUE);
-                    resp.getWriter().println(ret);
-                } else {
-                    resp.setStatus(HttpStatus.NOT_FOUND.value());
-                }
-            }
-        }).addMapping("/*"));
+        WebServer webServer = serverFactory.getWebServer(servletContext ->
+                servletContext.addServlet("dispatcherServlet",
+                        new DispatcherServlet(applicationContext) //스프링 컨테이너를 사용할 수 있도록 ApplicationContext 전달
+                ).addMapping("/*"));
         webServer.start();
     }
 }
