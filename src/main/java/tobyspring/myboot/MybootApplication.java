2faf5d7efe4dc106dda66fa7801bc77b453bae9d
박세원B <sewonpark@smilegate.sg>
2023-04-20 10:27:53 +0900

    add: main() 메서드 내부를 스프링부트와 유사하게 변경

--- src/main/java/tobyspring/myboot/MybootApplication.java
+++ src/main/java/tobyspring/myboot/MybootApplication.java
@@ -1,13 +1,13 @@
 package tobyspring.myboot;
 
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.DispatcherServlet;
 
+@Configuration
 @ComponentScan
 public class MybootApplication {
 
@@ -20,23 +20,8 @@ public class MybootApplication {
     public DispatcherServlet dispatcherServlet() {
         return new DispatcherServlet();
     }
+
     public static void main(String[] args) {
-        AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext() {
-            @Override
-            protected void onRefresh() {
-                super.onRefresh();
-
-                ServletWebServerFactory serverFactory = this.getBean(ServletWebServerFactory.class);
-                DispatcherServlet dispatcherServlet = this.getBean(DispatcherServlet.class);
-
-                WebServer webServer = serverFactory.getWebServer(servletContext ->
-                        servletContext.addServlet("dispatcherServlet", dispatcherServlet)
-                                .addMapping("/*"));
-                webServer.start();
-            }
-        };
-
-        applicationContext.register(MybootApplication.class);
-        applicationContext.refresh();
+        MySpringApplication.run(MybootApplication.class, args);
     }
 }
