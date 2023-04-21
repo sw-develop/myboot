a03aa9c6eec8100ce37627e32d0a73d6af515174
박세원B <sewonpark@smilegate.sg>
2023-04-21 16:37:35 +0900

    add: 인프라 빈 구성 정보 분리

---
+++ src/main/java/tobyspring/config/Config.java
@@ -0,0 +1,8 @@
+package tobyspring.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class Config {
+
+}
--- src/main/java/tobyspring/myboot/MybootApplication.java
+++ src/main/java/tobyspring/myboot/MybootApplication.java
@@ -1,26 +1,10 @@
 package tobyspring.myboot;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.DispatcherServlet;
+import tobyspring.config.MySpringBootApplication;
 
-@Configuration
-@ComponentScan
+@MySpringBootApplication
 public class MybootApplication {
-
-    @Bean
-    public ServletWebServerFactory serverFactory() {
-        return new TomcatServletWebServerFactory();
-    }
-
-    @Bean
-    public DispatcherServlet dispatcherServlet() {
-        return new DispatcherServlet();
-    }
 
     public static void main(String[] args) {
 //        MySpringApplication.run(MybootApplication.class, args);
